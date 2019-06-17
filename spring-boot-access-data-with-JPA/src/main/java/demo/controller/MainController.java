790ccd0142224d2c40b7f9e39709045789d10a97
grey0207 <grey0207@qq.com>
2019-06-17 15:43:44 +0800

    remove unused import

--- spring-boot-access-data-with-JPA/src/main/java/demo/controller/MainController.java
+++ spring-boot-access-data-with-JPA/src/main/java/demo/controller/MainController.java
@@ -5,8 +5,6 @@ import demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 public class MainController {
     @Autowired
